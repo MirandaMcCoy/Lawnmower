1094a9d827e05c4cf7a85ca13ba6f025b54fdb58
Miranda McCoy <mirandamccoy5@gmail.com>
2018-02-27 14:09:17 -0600

    LM-02 -- Test event created

---
+++ Lawnmower/Lawnmower/obj/Debug/android/src/md5b4e31651d657ad2fd182b15a340861c2/LoginViewHolder.java
@@ -0,0 +1,38 @@
+package md5b4e31651d657ad2fd182b15a340861c2;
+
+
+public class LoginViewHolder
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Lawnmower.Holders.LoginViewHolder, Lawnmower, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", LoginViewHolder.class, __md_methods);
+	}
+
+
+	public LoginViewHolder ()
+	{
+		super ();
+		if (getClass () == LoginViewHolder.class)
+			mono.android.TypeManager.Activate ("Lawnmower.Holders.LoginViewHolder, Lawnmower, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}

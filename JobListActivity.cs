af4b08a82ad4fcff9a8afaa02c6152f13ae1c1dc
MirandaMcCoy <mirandamccoy5@gmail.com>
2018-03-25 21:27:33 -0500

    master -- I know it's terrible to do this on master BUT I found the problem and fixed it and now everything should be groovy gravy - I hope

---
+++ JobListActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace Lawnmower
+{
+    [Activity(Label = "Lawnmower", ScreenOrientation = ScreenOrientation.Portrait, Theme = "@android:style/Theme.NoTitleBar")]
+    public class JobListActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.JobList);
+        }
+    }
+}
+
---
+++ LoginActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Lawnmower.ViewHolders;
+
+namespace Lawnmower
+{
+    [Activity(Label = "LoginActivity", ScreenOrientation = ScreenOrientation.Portrait, Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true)]
+    public class LoginActivity : Activity
+    {
+        private LoginViewHolder holder;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Login);
+
+            if (holder == null)
+            {
+                holder = new LoginViewHolder();
+            }
+
+            SetHolderViews();
+
+            AssignClickEvents();
+        }
+
+        protected override void OnDestroy()
+        {
+            UnassignClickEvents();
+
+            base.OnDestroy();
+        }
+
+        private void SetHolderViews()
+        {
+            holder.mainButton = FindViewById<Button>(Resource.Id.MainButton);
+        }
+
+        #region Click Events
+        private void AssignClickEvents()
+        {
+            holder.mainButton.Click += MainClick;
+        }
+
+        private void UnassignClickEvents()
+        {
+            holder.mainButton.Click -= MainClick;
+        }
+
+        private void MainClick(object sender, EventArgs e)
+        {
+            StartActivity(typeof(JobListActivity));
+        }
+#endregion
+    }
+}

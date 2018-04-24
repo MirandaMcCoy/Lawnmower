63f6f4ef8516461c983430c4d76947b479a74839
Miranda McCoy <mirandamccoy5@gmail.com>
2018-04-24 13:48:59 -0500

    create_job_branch - Checkin

---
+++ Shared.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Xamarin.Database;
+using Lawnmower.Objects;
+
+namespace Lawnmower
+{
+    static class Shared
+    {
+        public static string FirebaseURL = "https://lawnmower-a4296.firebaseio.com/";
+        public static FirebaseClient FirebaseClient = new FirebaseClient(FirebaseURL);
+        public static Job[] jobList;
+    }
+}
